95cceb898bb0ea0b5ce9db6df9070b72d3837dbe
Skonks <skinzeira@gmail.com>
2021-08-09 16:14:31 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/2.1 tipo-de-dados.js
+++ 3_1JS-rocketseat-GuiaEstelar/2.1 tipo-de-dados.js
@@ -11,4 +11,10 @@
         *Precisamos saber como escrever
         *Precisamos saber os significados
         *Precisamos continuar aprendendo, para crescer nosso vocabulário
+
+    Como vai ser a dinâmica do aprendizado?
+        *Conceitos e escrita
+
+    >Vamos aprender os tipos de dados mais utilizados na linguagem
+        *-Você sabia que é possível aprender mais de 80% de uma lingua nova, com cerca de apenas 20% do vocabulário? (Até menos)
 */
