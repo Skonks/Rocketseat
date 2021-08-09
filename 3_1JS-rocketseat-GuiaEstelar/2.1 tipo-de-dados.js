a71a77fa2944947ddd8a2f58a8ca1255f3bec22c
Skonks <skinzeira@gmail.com>
2021-08-09 16:12:32 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/2.1 tipo-de-dados.js
+++ 3_1JS-rocketseat-GuiaEstelar/2.1 tipo-de-dados.js
@@ -1,3 +1,14 @@
 /*
-
+    Tipo de dados
+        *Grámatica
+            *-Elementos de linguagem e suas combinações
+            *-A arte de falar e escrever corretamente
+        
+        *Vocabulário
+            *-Conjunto de termos e expressões
+            *-Agrupamento de palavras
+        
+        *Precisamos saber como escrever
+        *Precisamos saber os significados
+        *Precisamos continuar aprendendo, para crescer nosso vocabulário
 */
