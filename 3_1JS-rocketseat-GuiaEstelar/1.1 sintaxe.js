6455f604a12f041fb624f7b171dba7dfd209e72a
Skonks <skinzeira@gmail.com>
2021-08-09 15:48:02 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/1.1 sintaxe.js
+++ 3_1JS-rocketseat-GuiaEstelar/1.1 sintaxe.js
@@ -1,3 +1,9 @@
 /*
     Importância da Sintaxe
+        *Toda linguagem tem sintaxe
+        *Uma boa comunicação precisa de uma boa sintaxe
+        *82% dos erros em programação iniciante estão em sintaxe
 */
+
+//Js
+    console.log("Hello World")
