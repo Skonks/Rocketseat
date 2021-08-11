4631d105386aff78df842722a1738deaa7f0fb41
Skonks <skinzeira@gmail.com>
2021-08-11 13:25:57 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/2.8 conclusao-sintaxe.js
+++ 3_1JS-rocketseat-GuiaEstelar/2.8 conclusao-sintaxe.js
@@ -1,3 +1,28 @@
 /*
-    
+    Tipo de dados
+        Conforme o ECMAScript standard temos 9 tipos de dados:
+
+        *Data types
+            *-Primitive/Primitive value
+            *-Structural
+            *-Structural primitive
+
+        ?Primitivos:
+            *-String
+            *-Number
+            *-Boolean
+            *-undefined
+            *-Symbol
+            *-BigInt
+
+        ?Estruturais:
+            *-Array
+            *-Map
+            *-Set
+            *-Date
+            *-...
+        ?Function
+
+        ?Primitivo estrutural / Structural root primitive
+            *-null
 */
