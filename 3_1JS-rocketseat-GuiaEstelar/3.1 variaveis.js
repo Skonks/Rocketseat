233a5a255a178340cc7cf3d00d01a4351e844654
Skonks <skinzeira@gmail.com>
2021-08-11 16:13:22 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/3.1 variaveis.js
+++ 3_1JS-rocketseat-GuiaEstelar/3.1 variaveis.js
@@ -1,3 +1,20 @@
 /*
     Conhecendo as variáveis
+        *-Nomes simbólicos para receber algum valor
+        *-Atalhos de código
+        *-Identificadores
+
+    ?3 palavras reservadas para criar uma variável
+        *-var
+        *-let
+        *-const
 */
+
+var Yukie = "Linda"
+console.log(Yukie)
+
+let Chris = "JUSTICE!"
+console.log(Chris)
+
+const Margit = "Hasen Jagd!" // Não pode mudar
+console.log(Margit)
