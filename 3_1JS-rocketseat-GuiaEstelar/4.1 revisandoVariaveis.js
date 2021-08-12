88e727dbe753bd25503f5f867659fed99e36523b
Skonks <skinzeira@gmail.com>
2021-08-12 17:27:19 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/4.1 revisandoVariaveis.js
+++ 3_1JS-rocketseat-GuiaEstelar/4.1 revisandoVariaveis.js
@@ -1,10 +1,9 @@
 /*
     Variáveis e tipo de dados
 */
-// Declaração / Declaration
+// Declaração / Declaration | Agrupando declarações
 var nome
-let age
-let isHuman
+let age, isHuman
 
 // Assignments / Atribuição de dados
 nome = "Chris" // String
@@ -12,6 +11,4 @@ age = 20 // Number
 isHuman = true // Boolean
 
 // Que tipo de dado foi colocado na variavel
-console.log(typeof nome)
-console.log(typeof age)
-console.log(typeof isHuman)
+console.log(nome, age, isHuman)
