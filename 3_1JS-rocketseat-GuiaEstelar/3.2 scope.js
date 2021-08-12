2a2336549fa73c49c9191924c1be824e49e27f08
Skonks <skinzeira@gmail.com>
2021-08-12 15:12:59 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/3.2 scope.js
+++ 3_1JS-rocketseat-GuiaEstelar/3.2 scope.js
@@ -1,3 +1,13 @@
 /*
     Scope
+    *Escopo determina a visibilidade de alguma variável no JS
+
+    ?Block Statment
+    JS
+        Vamos iniciar um bloco:
+            {
+                Aqui dentro é um bloco e posso colocar qualquer código
+            } Aqui fechamos o bloco
+
+    O bloco também criará um novo escopo. Chamamos ele de `block-scoped`
 */
