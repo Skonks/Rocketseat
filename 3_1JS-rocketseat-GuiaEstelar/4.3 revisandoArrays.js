a09c70dab8aad916fd51c8eace6c94b74e39e886
Skonks <skinzeira@gmail.com>
2021-08-12 17:57:46 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/4.3 revisandoArrays.js
+++ 3_1JS-rocketseat-GuiaEstelar/4.3 revisandoArrays.js
@@ -2,3 +2,15 @@
     Arrays
 */
 
+const animals = [
+    "Mamaco",
+    "Ratinho",
+    {
+       name: "Gatinho",
+       age: 3
+    }
+]
+
+// Acessar valores dentro do array | Com objeto é possivel usar .name e etc.
+console.log(animals [2])
+console.log(animals.length)
