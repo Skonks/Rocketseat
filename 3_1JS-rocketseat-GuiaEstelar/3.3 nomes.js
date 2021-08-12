883d88dcb8e4b022204bc49dd8e125b60f1bad13
Skonks <skinzeira@gmail.com>
2021-08-12 17:08:34 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/3.3 nomes.js
+++ 3_1JS-rocketseat-GuiaEstelar/3.3 nomes.js
@@ -1,3 +1,23 @@
 /*
     Para criar nomes
+
+    *Js é case sensitive (Sensível ao caso)
+    *Js aceita a cadeira de caracteres Unicode
+
+    ?-Posso:
+        *-Iniciar com esses caracteres especiais: $ _
+        *-Iniciar com letras
+        *-Colocar acentos
+        *-Letras maíusculas e minúsculas fazem diferença
+
+    !-Não posso:
+        *-Iniciar com números
+        *-Colocar espaços vazios no nome
+
+    *-Ideal
+        *-Criar nomes que fazem sentido
+        *-Que explique o que a variável é ou faz
+        *-camelCase
+        *-snake_case
+        *-Escrever em inglês
 */
