3d8d603061083d5674d0645c9aa857f3933412b9
Skonks <skinzeira@gmail.com>
2021-08-12 17:20:50 -0300

    commit

---
+++ 3_1JS-rocketseat-GuiaEstelar/4.1 revisandoVariaveis.js
@@ -0,0 +1,11 @@
+/*
+    Variáveis e tipo de dados
+*/
+// Declaração / Declaration
+var name
+
+// Assignments / Atribuição de dados
+name = "Chris"
+
+// Que tipo de dado é esse?
+console.log(typeof name)
