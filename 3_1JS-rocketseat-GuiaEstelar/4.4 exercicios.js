4b23458e90e50bf747575f41f9ce04eebf6bcff3
Skonks <skinzeira@gmail.com>
2021-08-13 14:44:45 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/4.4 exercicios.js
+++ 3_1JS-rocketseat-GuiaEstelar/4.4 exercicios.js
@@ -3,7 +3,7 @@
 */
 
 // 1.Declare uma variável de nome weight e veja qual é o tipo da variável
-var weight
+var weight = 100.00
 console.log(typeof weight)
 
 // 2.Declare uma variável e atribua valores para cada um dos dados
@@ -11,14 +11,24 @@ console.log(typeof weight)
     name: String
     age: Number (integer)
     star: Number (float)
-    isSubscribed (boolean)
+    isSubscribed (boolean) 
 */
 
-const nome = "Yukie"
-const age = 18
-const star = "10"
-const isSubscribed = true
+let nome = "Yukie"
+let age = "18"
+let star = "5.0"
+let isSubscribed = false
 
 console.log(nome, age, star, isSubscribed)
 
-// Crie uma variável chamada student, veja qual 
+// Crie uma variável chamada student, veja qual é o tipo dela, atribua as mesmas propriedades e valores do exercicio acima, e mostre no console a seguinte mensagem : "name" de idade "age" pesa "weight" kg's
+
+let student = {
+    nome: "Chris",
+    age: 18,
+    star: 5.0,
+    weight: 60.00,
+    isSubscribed: true
+}
+
+console.log(`${student.nome} de idade ${student.age} pesa ${student.weight}`)
