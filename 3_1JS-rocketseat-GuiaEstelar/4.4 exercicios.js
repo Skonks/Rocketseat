d0438e843d976508a5b10fd2b88ff897ebb28908
Skonks <skinzeira@gmail.com>
2021-08-13 14:29:11 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/4.4 exercicios.js
+++ 3_1JS-rocketseat-GuiaEstelar/4.4 exercicios.js
@@ -1,3 +1,13 @@
 /*
     Exercícios
 */
+
+// Declare uma variável de nome weight e veja qual é o tipo da variável
+
+// Declare uma variável e atribua valores para cada um dos dados
+/*
+    name: String
+    age: Number (integer)
+    star: Number (float)
+    isSubscribed (boolean)
+*/
