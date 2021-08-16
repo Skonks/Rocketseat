79f3d3aa04b924db5b4f05f8909911a6003b8e4a
Skonks <skinzeira@gmail.com>
2021-08-16 18:13:08 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.3 function_scope.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.3 function_scope.js
@@ -3,10 +3,10 @@
 */
 
 let subject = "create video"
-function createThink(subject) {
-    subject = "study" // Não estamos sobreescrevendo a subject de fora
+function createThink(subject) { // Parâmetro
+    subject = "study" // Não estamos sobreescrevendo a subject de fora enquanto tivermos um parâmetro
     return subject
 }
 
-console.log(createThink(subject))
+console.log(createThink(subject)) // << Retorno da função e atualiza o objeto
 console.log(subject)
