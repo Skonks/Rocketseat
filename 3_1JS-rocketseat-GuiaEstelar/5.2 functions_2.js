b110a0b1f895818cd98aa6c130c443fea25cd015
Skonks <skinzeira@gmail.com>
2021-08-16 15:46:42 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
@@ -1,5 +1,5 @@
 /*
-    Functions
+    Functions (Sempre retorna algo)
 
     ?Pra que serve? (Agrupamento de código, reutilização e dar significado ao código)
 */
@@ -7,7 +7,15 @@
 
 // Parâmetros só vão funcionar dentro da função (number1 & number2) 
 const sum = function (number1, number2) {
-    console.log(number1 + number2)
+    total = number1 + number2
+    return total
 }
 
-sum(2, 3) // << Argumentos 
+//sum(2, 3) // << Argumentos 
+
+let number1 = 39
+let number2 = 50
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`A soma é ${sum(number1, number2)}`)
