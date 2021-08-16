a66f881b2f30d9dad71d16d86e85a66ea1a9c5dc
Skonks <skinzeira@gmail.com>
2021-08-16 15:33:50 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
@@ -3,4 +3,9 @@
 
     ?Pra que serve? (Agrupamento de código, reutilização e dar significado ao código)
 */
+// Função anônima / function expression
+const sum = function () {
+    console.log(1+1)
+}
 
+sum()
