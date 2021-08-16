0da8b51c72f4ae14e3532352aa2680c6ea00ed52
Skonks <skinzeira@gmail.com>
2021-08-16 15:25:33 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.1 functions.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.1 functions.js
@@ -4,3 +4,7 @@
     Pra que serve?
         *-Vamos supor que quero um aplicativo que tenha frases motivacionais
 */
+function createPhrases() {
+    console.log("Yukie Mayuzumi é linda e me da forças para continuar")
+    console.log("Christiane Friedrich é linda e me da forças para continuar")
+}
