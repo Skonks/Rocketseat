255932900b0e20ee533d966f71ee6c0648374b79
Skonks <skinzeira@gmail.com>
2021-08-16 15:31:06 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.1 functions.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.1 functions.js
@@ -1,10 +1,10 @@
 /*
     Functions
 
-    Pra que serve?
-        *-Vamos supor que quero um aplicativo que tenha frases motivacionais
+    ?Pra que serve? (Agrupamento de código, reutilização e dar significado ao código)
 */
 
+// *-Vamos supor que quero um aplicativo que tenha frases motivacionais
 // 1º Passo = Declaração da função
 function createPhrases() {
     console.log("Yukie Mayuzumi é linda e me da forças para continuar")
