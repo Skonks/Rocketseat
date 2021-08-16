503a05cbaf4366b8668dc95a0db6b70d9f913101
Skonks <skinzeira@gmail.com>
2021-08-16 15:28:07 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.1 functions.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.1 functions.js
@@ -4,7 +4,12 @@
     Pra que serve?
         *-Vamos supor que quero um aplicativo que tenha frases motivacionais
 */
+
+// 1º Passo = Declaração da função
 function createPhrases() {
     console.log("Yukie Mayuzumi é linda e me da forças para continuar")
     console.log("Christiane Friedrich é linda e me da forças para continuar")
 }
+
+// 2º Passo = Chamar a função
+createPhrases()
