0e345dcb7ea89769cbcbd36a3036e06432aa9604
Skonks <skinzeira@gmail.com>
2021-08-16 15:36:23 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
@@ -4,8 +4,9 @@
     ?Pra que serve? (Agrupamento de código, reutilização e dar significado ao código)
 */
 // Função anônima / function expression
-// Parâmetros
+// Parâmetros (number1 & number2)
 const sum = function (number1, number2) {
+    console.log(number1 + number2)
 }
 
 sum(2, 3)
