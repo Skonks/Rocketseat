7406142ce081d6f40079266fc9fabd62e9138051
Skonks <skinzeira@gmail.com>
2021-08-16 18:04:49 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
@@ -6,10 +6,10 @@
 // Função anônima / function expression
 
 // Parâmetros só vão funcionar dentro da função (fruta1 e fruta2) 
-function fazerSuco (fruta1, fruta2) {
+function fazerSuco(fruta1, fruta2) {
     return fruta1 + fruta2
 }
 
 //fazerSuco(2, 3) // << Argumentos 
-const copo = fazerSuco("banana", "maçã")
-console.log(copo)
+const copo = fazerSuco("banana", "maçã") // << Colocando coisas dentro da função
+console.log(copo) // << Recebendo as coisas transformadas
