d2384e3fc519212a423a2e8e62b90a09ce8ba03c
Skonks <skinzeira@gmail.com>
2021-08-16 17:58:20 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.2 functions_2.js
@@ -19,3 +19,4 @@ let number2 = 50
 console.log(`O número 1 é ${number1}`)
 console.log(`O número 2 é ${number2}`)
 console.log(`A soma é ${sum(number1, number2)}`)
+console.log(total) // Não fazer isso
