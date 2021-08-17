dd85eef65e60b017f05fd8a833ac2eb77961d8e8
Skonks <skinzeira@gmail.com>
2021-08-17 17:48:55 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/5.7 callback_function.js
+++ 3_1JS-rocketseat-GuiaEstelar/5.7 callback_function.js
@@ -1,3 +1,16 @@
 /*
-    Callback function
+    Callback function (Função sendo passada por uma função)
 */
+
+function sayMyName(name) {
+    console.log("Antes de executar o callback")
+    name()
+
+    console.log("Depois de executar o callback")
+}
+
+sayMyName(
+    () => {
+        console.log("Estou em uma callback")
+    }
+)
