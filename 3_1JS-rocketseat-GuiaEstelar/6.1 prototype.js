f39b576b0d7faeab0f71b41b63876ae44f9e5539
Skonks <skinzeira@gmail.com>
2021-08-18 02:28:50 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.1 prototype.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.1 prototype.js
@@ -3,5 +3,5 @@
 
     *Prototype-based language
     *Prototype chain
-    *_proto_ (Usar no navegador para ver, mas basicamente ele mostra tudo que pode ser usado em algo que você quer usar) | (Usar {} ou "Skonks"._proto_) | (Para ver number tem que usar float)
+    *__proto__ (Usar no navegador para ver, mas basicamente ele mostra tudo que pode ser usado em algo que você quer usar) | (Usar {} ou "Skonks"._proto_) | (Para ver number tem que usar float)
 */
