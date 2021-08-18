ce86ec6f39e30a0f0ae53d60a85b788e90213c66
Skonks <skinzeira@gmail.com>
2021-08-18 02:50:58 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.2 type_coersion.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.2 type_coersion.js
@@ -2,4 +2,5 @@
     Type conversion (Eu fazendo a troca) vs Type coersion (JS fazendo a troca)
     *Alteração de um tipo de dado para outro tipo
 */
-console.log("9" + 5)
+console.log("9" + 5) // Coersion, o JS tá transformando o 5 em String
+console.log(Number("9") + 5) // Conversion, eu estou trocando
