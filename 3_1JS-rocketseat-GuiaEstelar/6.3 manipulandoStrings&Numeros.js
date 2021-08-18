d8289cfe359e8f74138d755de0e1d60bfc2dbeb4
Skonks <skinzeira@gmail.com>
2021-08-18 02:58:08 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.3 manipulandoStrings&Numeros.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.3 manipulandoStrings&Numeros.js
@@ -1,17 +1,21 @@
 /*
     Manipulando Strings e Numbers
 
-    Transformar número em string e string em número
 */
+//Transformar número em string e string em número
 let string = "5"
 console.log(Number(string))
 
 let number = 3
 console.log(String(number))
 
+// ================================================================================================================
 // Quero contar quantos caracteres tem uma palavra e quantos digitos tem um número
 let word = "Paralelepipedo"
 console.log(word.length)
 
 let numero = 1234
 console.log(String(numero).length) // Transformando em string para ver quantos números tem
+
+// ================================================================================================================
+//Transformar um número quebrado com 2 casa decimais e trocar ponto por vírgula
