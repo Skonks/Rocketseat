73a359b13823a3246666391293839efbaf2e07e8
Skonks <skinzeira@gmail.com>
2021-08-18 02:48:53 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.2 type_coersion.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.2 type_coersion.js
@@ -1,4 +1,4 @@
 /*
-    Type conversion (typecasting) vs Type coersion
+    Type conversion (Eu fazendo a troca) vs Type coersion (JS fazendo a troca)
     *Alteração de um tipo de dado para outro tipo
 */
