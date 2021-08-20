83f0cd16ded594a43875e50808b40fcf05aaca93
Skonks <skinzeira@gmail.com>
2021-08-19 22:53:56 -0300

    commit

---
+++ 3_1JS-rocketseat-GuiaEstelar/6.4 scripts.js
@@ -0,0 +1,9 @@
+/*
+    Adicionar um item no fim
+    Adicionar no começo
+    Remover do fim
+    Remover do começo
+    Pegar somente alguns elementos do array
+    Remover 1 ou mais itens em qualquer posição do array
+    Encontrar a posição de um elemento no array
+*/
