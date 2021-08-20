15361df96c139744563cb324700dd01a97b8115d
Skonks <skinzeira@gmail.com>
2021-08-20 19:50:27 -0300

    commit

---
+++ 3_1JS-rocketseat-GuiaEstelar/7.6 operadoresComparacao.js
@@ -0,0 +1,16 @@
+/* 
+    Operadores de comparação
+    Irá comparar valores retornando um booleano como resposta
+*/
+
+let one = 1
+let two = 2
+
+// == igual a 
+console.log(two == 1)
+console.log(one == "1")
+
+// != diferente de
+console.log(one != two)
+console.log(one != 1)
+console.log(one != "1")
