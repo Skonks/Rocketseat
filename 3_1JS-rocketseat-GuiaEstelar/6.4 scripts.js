4f4c161b6c9e89d7b9f064d4d83211f951c7aa95
Skonks <skinzeira@gmail.com>
2021-08-19 22:57:13 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.4 scripts.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.4 scripts.js
@@ -1,9 +1,24 @@
-/*
-    Adicionar um item no fim
-    Adicionar no começo
-    Remover do fim
-    Remover do começo
-    Pegar somente alguns elementos do array
-    Remover 1 ou mais itens em qualquer posição do array
-    Encontrar a posição de um elemento no array
-*/
+let techs = ["yukie", "chris", "margit"]
+
+//Adicionar um item no fim
+console.log(techs.push("benkei"))
+
+//Adicionar no começo
+
+
+//Remover do fim
+
+
+//Remover do começo
+
+
+//Pegar somente alguns elementos do array
+
+
+//Remover 1 ou mais itens em qualquer posição do array
+
+
+//Encontrar a posição de um elemento no array
+
+
+console.log(techs)
