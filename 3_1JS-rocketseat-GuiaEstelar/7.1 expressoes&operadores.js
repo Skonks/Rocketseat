df3acfd54db0f4cc3a436269609625cc954e0cd2
Skonks <skinzeira@gmail.com>
2021-08-19 23:22:58 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/7.1 expressoes&operadores.js
+++ 3_1JS-rocketseat-GuiaEstelar/7.1 expressoes&operadores.js
@@ -1,9 +1,18 @@
 /* 
     Expressões e Operadores
 
-        -Expressions
+        -Expressions (Qualquer linha de código que resolve algo no JS)
         -Operators
             *-Binary
             *-Unary
             *-Ternary
 */
+
+//Expressões
+let number = 1; // Vai dar erro porque a função vai pensar que o let number = 1 é parte da função, ai tem que colocar virgula
+
+(function () {
+    console.log("Yukie linda")
+})()
+
+//Operadores
