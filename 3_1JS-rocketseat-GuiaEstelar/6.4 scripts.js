42318a8c19dea5b80c8f39ba09be3496948f6871
Skonks <skinzeira@gmail.com>
2021-08-19 23:08:18 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.4 scripts.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.4 scripts.js
@@ -13,11 +13,11 @@ techs.pop()
 techs.shift()
 
 //Pegar somente alguns elementos do array
-console.log(techs.slice(1, 3))
+//console.log(techs.slice(1, 3))
 
 //Remover 1 ou mais itens em qualquer posição do array
-
+techs.splice(1)
 
 //Encontrar a posição de um elemento no array
 
-//console.log(techs)
+console.log(techs)
