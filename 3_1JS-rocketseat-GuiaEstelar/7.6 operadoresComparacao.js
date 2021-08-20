dca3e01d110f953fdace6c57f0a05cbe7d48dd35
Skonks <skinzeira@gmail.com>
2021-08-20 19:59:13 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/7.6 operadoresComparacao.js
+++ 3_1JS-rocketseat-GuiaEstelar/7.6 operadoresComparacao.js
@@ -26,3 +26,26 @@ console.log(one === 1)
 // !== estritamente diferente de
 console.log(two !== "2")
 console.log(two !== 2)
+
+
+/* 
+    Operadores de comparação maior e menor (igual)
+*/
+
+let um = 1
+let dois = 2
+
+// > maior que
+console.log(um > dois)
+
+// >= maior ou igual a
+console.log(um >= 1)
+console.log(two >= 1)
+
+// < menor que
+console.log(one < two)
+
+// <= menor ou igual a
+console.log(one <= two)
+console.log(one <= 1)
+console.log(one <= 0)
