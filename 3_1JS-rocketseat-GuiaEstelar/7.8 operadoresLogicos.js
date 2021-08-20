dc6037af142da772c56f996af059d22597b52975
Skonks <skinzeira@gmail.com>
2021-08-20 20:17:03 -0300

    commit

---
+++ 3_1JS-rocketseat-GuiaEstelar/7.8 operadoresLogicos.js
@@ -0,0 +1,13 @@
+/* 
+    Operadores lógicos (logical operators)
+*/
+
+// - 2 valores booleanos, quando verificados, resultará em verdadeiro ou falso
+let pao = true
+let queijo = true
+
+// and = &&
+
+// or = ||
+
+// not = !
