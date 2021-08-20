08904b22a29b0aa16a8d17ff2ceec1779cd91b42
Skonks <skinzeira@gmail.com>
2021-08-20 19:28:34 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/7.3 operadoresUnarios.js
+++ 3_1JS-rocketseat-GuiaEstelar/7.3 operadoresUnarios.js
@@ -3,3 +3,11 @@
         *-typeof
         *-delete
 */
+const person = {
+    name: "Yukie",
+    age: "18"
+}
+
+delete person.age
+console.log(person) // Vai deletar uma propriedade dentro do objeto
+console.log(typeof "Christiane")
