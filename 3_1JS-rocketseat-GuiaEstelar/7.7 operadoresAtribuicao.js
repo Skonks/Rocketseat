514ed558f2d6afe3eceeaa0721b56aa4ac0e8de2
Skonks <skinzeira@gmail.com>
2021-08-20 20:04:02 -0300

    commit

---
+++ 3_1JS-rocketseat-GuiaEstelar/7.7 operadoresAtribuicao.js
@@ -0,0 +1,25 @@
+/* 
+    Operadores de atribuição (Assignment)
+*/
+var x
+console.log(x)
+
+// Assignment
+x = 1
+console.log(x)
+
+// Addition assignment
+x += 2
+console.log(x)
+
+// Subtraction assignment
+x -= 1
+console.log(x)
+
+// Multiplication assignment
+x *= 2
+console.log(x)
+
+// Division assignment
+x /= 2
+console.log(x)
