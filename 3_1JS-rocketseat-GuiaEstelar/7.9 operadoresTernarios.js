9e0dc36d8754145329d8d062167f57290b5527a2
Skonks <skinzeira@gmail.com>
2021-08-23 10:04:17 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/7.9 operadoresTernarios.js
+++ 3_1JS-rocketseat-GuiaEstelar/7.9 operadoresTernarios.js
@@ -1,14 +1,17 @@
 /* 
     Operador condicional (ternário)
+    Dependendo da condição, recebemos valores diferentes
+
+    Condição então valor 1 se não, valor 2
+        condition ? value1 : value2
+
+Exemplos: 
 */
 
-// Dependendo da condição, recebemos valores diferentes
-
-// Condição então valor 1 se não, valor 2
-// condition ? value1 : value2
-
-// Exemplos: 
-
 // Café da manhã top
+let pao = false
+let queijo = true
+const niceBreakfast = pao || queijo ? "Café doido" : "Café feio"
+console.log(niceBreakfast)
 
 // Maior que 18
