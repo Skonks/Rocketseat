dac64219637c58f150753ffac292607ecfedc946
Skonks <skinzeira@gmail.com>
2021-08-23 10:21:24 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/6.2 type_coersion.js
+++ 3_1JS-rocketseat-GuiaEstelar/6.2 type_coersion.js
@@ -4,3 +4,31 @@
 */
 console.log("9" + 5) // Coersion, o JS tá transformando o 5 em String
 console.log(Number("9") + 5) // Conversion, eu estou trocando
+
+/* 
+    FALSY
+        Quando um valor é considerado false em contextos aonde um booleano é obrigatório (condicionais e loop)
+            false
+            0
+            -0
+            ""
+            null
+            undefined
+            NaN
+*/
+
+
+/* 
+    TRUTHY
+        Quando um valor é considerado true em contextos aonde um booleano é obrigatório (condicionais e loop)
+            true
+            {}
+            []
+            1
+            3.23
+            "0"
+            "false"
+            -1
+            Infinity
+            -Infinity
+*/
