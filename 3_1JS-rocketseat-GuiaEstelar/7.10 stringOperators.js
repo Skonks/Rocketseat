8be9a29602daf4626273cf8349274d87ff1cb0a1
Skonks <skinzeira@gmail.com>
2021-08-23 10:17:39 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/7.10 stringOperators.js
+++ 3_1JS-rocketseat-GuiaEstelar/7.10 stringOperators.js
@@ -10,4 +10,5 @@ console.log("Lin"+"da")
 
 // Retorna a união de duas Strings
 let alpha = "alpha"
+// alpha += "bet" (É bom caso eu queira só atualizar a string direto(alpha = alpha + "bet"))
 console.log(alpha + "bet")
