a2c4b0a15b98d944eaca21458d589e49f784ede6
Skonks <skinzeira@gmail.com>
2021-08-23 12:18:46 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/8.4 throw&catch.js
+++ 3_1JS-rocketseat-GuiaEstelar/8.4 throw&catch.js
@@ -1,3 +1,17 @@
 // throw
+function sayMyName(name = "") {
+    if (name === "") {
+        throw "Nome é obrigatório" // Vai parar a execução
+    }
 
-// try...catch
+    console.log(name)
+}
+
+// try...catch (Vai capturar o erro e vai continuar a aplicação)
+try { // Tentando a função
+    sayMyName("Yukie")
+} catch (e) { // Capturando o erro (?)
+    console.log(e)
+}
+
+console.log("Após o try/catch")
