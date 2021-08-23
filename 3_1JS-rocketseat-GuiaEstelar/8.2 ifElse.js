e44b529d3ae4909f13c79e8bfc663b26f88ae1ae
Skonks <skinzeira@gmail.com>
2021-08-23 11:42:09 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/8.2 ifElse.js
+++ 3_1JS-rocketseat-GuiaEstelar/8.2 ifElse.js
@@ -2,14 +2,16 @@
     if ... else
 */
 // if (condicional) {bloco de código}
-//Medir febre
-let temperature = 37.1
+// Medir febre
+let temperature = 36.9
+let highTemperature = temperature >=37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
 
 if (temperature === 37) {
     console.log("Está com febre")
-} else if (temperature < 37.5 && temperature >= 37) {
+} else if (mediumTemperature) {
     console.log("Febre normal")
-} else if (temperature >= 38) {
+} else if (highTemperature) {
     console.log("Febre alta")
 } else {
     console.log("Não está com febre")
