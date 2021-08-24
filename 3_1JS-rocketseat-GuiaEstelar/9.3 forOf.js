cb7e710b7f7b1882d0e258c6f2e40ed2bbcf6fae
Skonks <skinzeira@gmail.com>
2021-08-24 11:38:25 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/9.3 forOf.js
+++ 3_1JS-rocketseat-GuiaEstelar/9.3 forOf.js
@@ -1,3 +1,9 @@
 /* 
     Estrutura de repetição for...of
 */
+let name = "Yukie"
+let names = ["Chris", "Margit"]
+
+for (let name of names) { // Imprimindo uma letra a cada for
+    console.log(name)
+}
