d702dd03776c6d7dafef14fbfb2ea65d267ad55c
Skonks <skinzeira@gmail.com>
2021-08-24 11:33:07 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/9.2 while.js
+++ 3_1JS-rocketseat-GuiaEstelar/9.2 while.js
@@ -1,3 +1,8 @@
 /* 
-    Estrutura de repetição while
+    Estrutura de repetição while (Faz sentido usar quando a gente não sabe qual é o momento da parada)
 */
+let i = 0;
+while (i < 10) {
+    console.log(i)
+    i++
+}
