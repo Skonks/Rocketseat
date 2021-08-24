dea30d6deb61b6956fa344086595baef60b3a8a9
Skonks <skinzeira@gmail.com>
2021-08-24 14:39:05 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/10.2 exercicio_2.js
+++ 3_1JS-rocketseat-GuiaEstelar/10.2 exercicio_2.js
@@ -10,5 +10,10 @@
     Agora, crie uma função que irá calcular o total de receitas e despesas e irá mostrar uma mensagem se a familia está com saldo positivo ou negativo, seguido do valor do saldo
 */
 let gastosFamiliar = {
-    receitas: ["Receita1", "Receita2"], despesas: [100, 200]
+    incomes: [100, 200, 4000, 300],
+    expenses: [200, 100, 300, 300]
+}
+
+function balance(incomes, expenses) {
+    console.log("Teste")
 }
