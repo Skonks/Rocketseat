eab47b831f2db4ceab1a802ed76cefd6de11073e
Skonks <skinzeira@gmail.com>
2021-08-24 11:23:08 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/9.1 for.js
+++ 3_1JS-rocketseat-GuiaEstelar/9.1 for.js
@@ -1,9 +1,9 @@
 /* 
     Estrutura de repetição for
 */
-for (let i = 100; i > 0; i--) { // Iniciando variavel, condição de continuação do loop e por ultimo, expressão final para terminar o loop, incrementando de 1 em 1
-    if (i === 50) {
-        break;
+for (let i = 10; i > 0; i--) { // Iniciando variavel, condição de continuação do loop e por ultimo, expressão final para terminar o loop, incrementando de 1 em 1
+    if (i === 5) {
+        continue; // Ignorando o 5
     }
     console.log(i)
 }
