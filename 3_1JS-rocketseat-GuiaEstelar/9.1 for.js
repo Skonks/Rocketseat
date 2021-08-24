f276db0465ad199853903fecd0625f3a7bf457f1
Skonks <skinzeira@gmail.com>
2021-08-24 11:24:05 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/9.1 for.js
+++ 3_1JS-rocketseat-GuiaEstelar/9.1 for.js
@@ -3,7 +3,7 @@
 */
 for (let i = 10; i > 0; i--) { // Iniciando variavel, condição de continuação do loop e por ultimo, expressão final para terminar o loop, incrementando de 1 em 1
     if (i === 5) {
-        continue; // Ignorando o 5
+        continue; // break: Para a execução do loop. continue: Pula a execução do momento
     }
     console.log(i)
 }
