bc3763ba8682639f70330e9e798db245a0e6eb9f
Skonks <skinzeira@gmail.com>
2021-08-24 11:46:44 -0300

    commit

---
+++ 3_1JS-rocketseat-GuiaEstelar/10.1 exercicio_1.js
@@ -0,0 +1,12 @@
+/* 
+    Exercicio 1
+
+    Transformar notas escolares
+
+    Crie um algoritimo que transforme as notas do sistema número para sistema de notas em caractere tipo A B C (if else)
+    *-De 90 para cima = A
+    *-Entre 80 - 89 = B
+    *-Entre 70 - 79 = C
+    *-Entre 60 - 69 = D
+    *-Menor que 60 = F
+*/
