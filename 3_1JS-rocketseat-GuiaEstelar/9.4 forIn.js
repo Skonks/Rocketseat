1a8c7e396fd223c89ded7062e3d8d1b9cbffe14a
Skonks <skinzeira@gmail.com>
2021-08-24 11:41:40 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/9.4 forIn.js
+++ 3_1JS-rocketseat-GuiaEstelar/9.4 forIn.js
@@ -1,3 +1,12 @@
 /* 
-    Estrutura de repetição for...in
+    Estrutura de repetição for...in (Criando um loop em cima de um objeto, pegando as propriedades do objeto)
 */
+let person = {
+    name: "Chris",
+    age: 19,
+    weight: 50
+}
+
+for (let property in person) { // Pega as coisas que estão dentro de person e coloca em property e faz um loop até acabar o que mostrar
+    console.log(property)
+}
