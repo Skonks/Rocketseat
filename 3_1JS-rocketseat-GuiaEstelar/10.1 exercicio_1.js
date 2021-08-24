cf16993007d04256b009c4fb4059f5e5417b4c75
Skonks <skinzeira@gmail.com>
2021-08-24 12:44:52 -0300

    commit

--- 3_1JS-rocketseat-GuiaEstelar/10.1 exercicio_1.js
+++ 3_1JS-rocketseat-GuiaEstelar/10.1 exercicio_1.js
@@ -10,3 +10,43 @@
     *-Entre 60 - 69 = D
     *-Menor que 60 = F
 */
+let notaA = 91
+let notaB = 85
+let notaC = 75
+let notaD = 65
+let notaF = 59
+
+if (notaA > 90) {
+    notaA = "A"
+    console.log("A nota 1 virou: " + notaA)
+} else {
+    console.log("A nota não é valida")
+}
+
+if (notaB >= 80 && notaB <= 89) {
+    notaB = "B"
+    console.log("A nota 2 virou: " + notaB)
+} else {
+    console.log("A nota não é valida")
+}
+
+if (notaC >= 70 && notaC <= 79) {
+    notaC = "C"
+    console.log("A nota 3 virou: " + notaC)
+} else {
+    console.log("A nota não é valida")
+}
+
+if (notaD >= 60 && notaD <= 69) {
+    notaD = "D"
+    console.log("A nota 4 virou: " + notaD)
+} else {
+    console.log("A nota não é valida")
+}
+
+if (notaF < 60) {
+    notaF = "F"
+    console.log("A nota 5 virou: " + notaF)
+} else {
+    console.log("A nota não é valida")
+}
